dd36815deedfc83e5b58fc329b868ae2bcfa7921
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2024-07-21 10:18:56 +0530

    Created Next App

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full h-20 dark:bg-blue-950 text-slate-100'>
+        
+    </div>
+  )
+}
+
+export default Navbar
